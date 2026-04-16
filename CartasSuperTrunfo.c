211e6a6fdbbf136b052f4191ec473623ba183a04
Kauã Souza <kaua26323@gmail.com>
2026-04-16 15:06:20 -0400

    implementado a lógica de comparação de cartas com if e else

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,8 @@ int main() {
   char state;
   char cardCode[5];
   char city[60];
-  double pib;
+  double pib, superPower;
+  unsigned int cardResult;
   unsigned long int populationNumber;
   float areaInKm, populationDensity, pibPerCapita;
 
@@ -14,7 +15,8 @@ int main() {
   char state2;
   char cardCode2[5];
   char city2[60];
-  double pib2;
+  double pib2, superPower2;
+  unsigned int cardResult2;
   unsigned long int populationNumber2;
   float areaInKm2, populationDensity2, pibPerCapita2;
 
@@ -114,26 +116,118 @@ int main() {
 
   printf("===========================================\n");
 
-  double superPower = (double)populationNumber + areaInKm + pib + numberOfTouristAttractions + pibPerCapita + (1.0 / populationDensity);
-  double superPower2 = (double)populationNumber2 + areaInKm2 + pib2 + numberOfTouristAttractions2 + pibPerCapita2 + (1.0 / populationDensity2);
+  superPower = (double)populationNumber + areaInKm + pib + numberOfTouristAttractions + pibPerCapita + (1.0 / populationDensity);
+  superPower2 = (double)populationNumber2 + areaInKm2 + pib2 + numberOfTouristAttractions2 + pibPerCapita2 + (1.0 / populationDensity2);
 
-  char *winner[] = {"Carta 2", "Carta 1"};
-  short int resultPopulation = populationNumber > populationNumber2;
-  short int resultAreaInKm = areaInKm > areaInKm2;
-  short int resultAttractions =  numberOfTouristAttractions > numberOfTouristAttractions2;
-  short int resultDensity = populationDensity < populationDensity2;
-  short int resultPibPerCapita = pibPerCapita > pibPerCapita2;
+
 
   printf("--- Duelo das cartas ---\n");
-  printf("População: %s venceu!\n", winner[resultPopulation]);
-  printf("Área em Km²: %s venceu!\n", winner[resultAreaInKm]);
-  printf("PIB: %s venceu!\n", winner[pib > pib2]); 
-  printf("Pontos Turísticos: %s venceu!\n", winner[resultAttractions]);
-  printf("Densidade Populacional: %s venceu!\n", winner[resultDensity]);
-  printf("PIB per Capita: %s venceu!\n", winner[resultPibPerCapita]);
-  printf("Super Poder: %s venceu!\n", winner[superPower > superPower2]);
 
+  printf("População: ");
+  if( populationNumber > populationNumber2){
+    printf("Carta %d - (%s) Venceu!\n", card, city);
+    cardResult += 1;
+  } else {
+    if( populationNumber == populationNumber2){
+      printf("Empate!! as duas cartas são iguais!\n");
+    } else {
+      printf("Carta %d - (%s) Venceu!\n", card2, city2);
+      cardResult2 += 1;
+    }
+  }
 
+  printf("Área em Km²: ");
+  if( areaInKm > areaInKm2){
+    printf("Carta %d - (%s) Venceu!\n", card, city);
+    cardResult += 1;
+  }  else {
+    if( areaInKm == areaInKm2){
+      printf("Empate!! as duas cartas são iguais!\n");
+    } else {
+      printf("Carta %d - (%s) Venceu!\n", card2, city2);
+      cardResult2 += 1;
+    }
+  }
 
+  printf("PIB: "); 
+  if( pib > pib2){
+    printf("Carta %d - (%s) Venceu!\n", card, city);
+    cardResult += 1;
+  } else {
+    if( pib == pib2){
+      printf("Empate!! as duas cartas são iguais!\n");
+    } else {
+      printf("Carta %d - (%s) Venceu!\n", card2, city2);
+      cardResult2 += 1;
+    }
+  }
+
+  printf("Pontos Turísticos: ");
+  if( numberOfTouristAttractions > numberOfTouristAttractions2){
+    printf("Carta %d - (%s) Venceu!\n", card, city);
+    cardResult += 1;
+  } else {
+    if( numberOfTouristAttractions == numberOfTouristAttractions2){
+      printf("Empate!! as duas cartas são iguais!\n");
+    } else {
+      printf("Carta %d - (%s) Venceu!\n", card2, city2);
+      cardResult2 += 1;
+    }
+  }
+
+  printf("Densidade Populacional: ");
+  if( populationDensity < populationDensity2){
+    printf("Carta %d - (%s) Venceu!\n", card, city);
+    cardResult += 1;
+  } else {
+    if( populationDensity ==  populationDensity2 ){
+      printf("Empate!! as duas cartas são iguais!\n");
+    } else {
+      printf("Carta %d - (%s) Venceu!\n", card2, city2);
+      cardResult2 += 1;
+    }
+  }
+
+  printf("PIB per Capita: ");
+  if( pibPerCapita > pibPerCapita2){
+    printf("Carta %d - (%s) Venceu!\n", card, city);
+    cardResult += 1;
+  } else {
+    if( pibPerCapita ==  pibPerCapita2 ){
+      printf("Empate!! as duas cartas são iguais!\n");
+    } else {
+      printf("Carta %d - (%s) Venceu!\n", card2, city2);
+      cardResult2 += 1;
+    }
+  }
+ 
+  printf("Super Poder: ");
+  if( superPower > superPower2){
+    printf("Carta %d - (%s) Venceu!\n", card, city);
+    cardResult += 1;
+  } else {
+    if( superPower ==  superPower2 ){
+      printf("Empate!! as duas cartas são iguais!\n");
+    } else {
+      printf("Carta %d - (%s) Venceu!\n", card2, city2);
+      cardResult2 += 1;
+    }
+  }
+
+  printf("--- Placar Final ---\n");
+  printf("Carta %d - Pontos: %u\n", card, cardResult);
+  printf("Carta %d - Pontos: %u\n", card2, cardResult2);
+
+  printf("Resultado: ");
+  if(cardResult > cardResult2){
+    printf("Carta %d - (%s) Venceu!\n", card, city);
+  } else {
+    if(cardResult == cardResult2){
+      printf("!!EMPATE!!");
+    } else{
+      printf("Carta %d - (%s) Venceu!\n", card2, city2);
+    }
+  }
+  
 return 0;
 } 
